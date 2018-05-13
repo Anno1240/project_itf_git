7717ece846c139689873ca3d7c6e450889e3cb24
Farshad <r0583936@student.thomasmore.be>
2018-05-13 23:16:08 +0200

    Frontpage af (enkel nog 403/404) + validatie

--- registratie.php
+++ registratie.php
@@ -1,66 +1,62 @@
 <html>
 <head>
-<title>Contactformulier</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Hashbreakers: Professionele webdesigners voor aanmaak van Onepage website met Bootstrap">
-    <meta name="author" content="Farshad Nour, Dries Mangelschots">
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <title>HashBreakers</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Registratie</title>
 
-    <!--favicon -->
-    <link rel="shortcut icon" href="/favicon.png"/>
-    <!-- font awesome-->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+     <!--Bootstrap CSS + meta viewport-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+        <link rel="stylesheet" href="public_html/css/style.css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-    <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+        <!--icon libary-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- Custom styles for this template -->
-    <!--<link href="css/freelancer.min.css" rel="stylesheet">-->
-    <link href="css/freelancer.css" rel="stylesheet">
-    </head>
-<body class= "bg-blok1">
+        <!-- Real favicon-->
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
+<body>
 
 <!-- Responsieve navigatiebalk 3 breekpunten-->
-<nav class="navbar navbar-default navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Battle of Wisdom</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto bg-light">
-            <li class="nav-item">
-                <a class="nav-link" href="#titel">Over Battle of Wisdom</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#artikel">Artikels</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#login">Registreer</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#tools">Interessante tools</a>
-            </li>
-        </ul>
+<nav class="navbar navbar-default navbar-expand-lg navbar-dark bg-myblack">
+    <div class="container bg-myblack txt-mygreennav">
+        <a class="navbar-brand txt-mygreennav" href="public_html/index.html#">Battle of Wisdom</a>
+        <button class="navbar-toggler txt-mygreennav bg-myblack" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse float-right" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto bg-light float-right bg-myblack">
+                <li class="nav-item bg-myblack">
+                    <a class="nav-link txt-mygreennav" href="public_html/index.html#titel">Over Battle of Wisdom</a>
+                </li>
+                <li class="nav-item bg-myblack txt-mygreennav">
+                    <a class="nav-link txt-mygreennav" href="public_html/index.html#artikel">Artikels</a>
+                </li>
+                <li class="nav-item bg-myblack txt-mygreennav">
+                    <a class="nav-link txt-mygreennav" href="public_html/index.html#tools">Interessante tools</a>
+                </li>
+                <li class="nav-item bg-myblack txt-mygreennav">
+                    <a class="nav-link txt-mygreennav" href="public_html/index.html#login">Registreer</a>
+                </li>
+            </ul>
+
+        </div>
     </div>
+
 </nav>
 
 
-<h1 class="text-uppercase">Welkom Wisdom Warrior!</h1>
-<p class="font-italic"> Uw registratie werd succesvol geregistreerd</p></div>
-
-<div class="container float-left" >
+<div class="container" id="succes">
 
 
 <?php
@@ -83,7 +79,7 @@ if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL) || empty($_POST['email']))
    	echo '<p> De ingevulde informatie is:</p>';
     echo '<ul>';
     echo '<li> Naam: '. $_POST['name'] . '</li>';
-    echo '<li> Naam: '. $_POST['wachtwoord'] . '</li>';
+    echo '<li> Wachtwoord: '. $_POST['wachtwoord'] . '</li>';
     echo '<li> Email: ' . $_POST['email'] . '</li>';
 
     if(isset($_POST['interesse1'])) {
